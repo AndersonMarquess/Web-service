213139b13befc64025aee2a771fa4de6b570a74c
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-02-14 18:14:42 -0200

    Criado servidor com Grizzly

---
+++ src/main/java/com/andersonmarques/loja/ServidorMain.java
@@ -0,0 +1,21 @@
+package com.andersonmarques.loja;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class ServidorMain {
+	
+	public static void main(String[] args) throws IOException {
+		URI uriRaiz = URI.create("http://localhost:8080/");
+		/*Configura o grizzly para buscar os recursos como jax-rs e o utiliza como serviço*/
+		ResourceConfig config = new ResourceConfig().packages("com.andersonmarques.loja");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uriRaiz, config);
+		System.out.println("Servidor rodando - aperte qualquer tecla para desligar.");
+		System.in.read();
+		server.shutdown();
+	}
+}
