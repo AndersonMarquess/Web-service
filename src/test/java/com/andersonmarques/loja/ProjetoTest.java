05197235585c436e8c5bb4319c304f2115e019f1
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-02-15 15:55:15 -0200

    Respondendo projeto como JSON

--- src/test/java/com/andersonmarques/loja/ProjetoTest.java
+++ src/test/java/com/andersonmarques/loja/ProjetoTest.java
@@ -1,6 +1,6 @@
 package com.andersonmarques.loja;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -10,6 +10,9 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.andersonmarques.loja.model.Projeto;
+import com.google.gson.Gson;
 
 public class ProjetoTest {
 
@@ -31,6 +34,7 @@ public class ProjetoTest {
 		Client cliente = ClientBuilder.newClient();
 		WebTarget alvoRaiz = cliente.target("http://localhost:8081");
 		String resultado = alvoRaiz.path("/projeto/1").request().get(String.class);
-		assertTrue(resultado.contains("Minha loja"));
+		Projeto projeto = new Gson().fromJson(resultado, Projeto.class); 
+		assertEquals("Minha loja", projeto.getNome());
 	}
 }
