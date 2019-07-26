676a6928e2b10f0790b1e4004df72571cc01939d
unknown <andersongabriel96.ag@gmail.com>
2019-07-26 18:36:03 -0300

    Alteração do método de request para listagem de itens

--- JAX-WS/estoquews/src/main/java/com/andersonmarques/estoquews/ws/EstoqueWS.java
+++ JAX-WS/estoquews/src/main/java/com/andersonmarques/estoquews/ws/EstoqueWS.java
@@ -1,11 +1,19 @@
 package com.andersonmarques.estoquews.ws;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
+import com.andersonmarques.estoquews.models.item.Filtro;
+import com.andersonmarques.estoquews.models.item.Filtros;
+import com.andersonmarques.estoquews.models.item.Item;
 import com.andersonmarques.estoquews.models.item.ItemDao;
-import com.andersonmarques.estoquews.models.item.ListaItens;
 
 /* Anotação para criar o serviço web */
 @WebService
@@ -14,9 +22,17 @@ public class EstoqueWS {
 	private ItemDao itemDAO = new ItemDao();
 
 	@WebMethod(operationName = "todosOsItens")
-	@WebResult(name = "itens")
-	public ListaItens getItens() {
-		System.out.println("Buscando todos os itens");
-		return new ListaItens(this.itemDAO.todosItens());
+	@RequestWrapper(localName = "listaItens")
+	// Involucro geral
+	@ResponseWrapper(localName = "itens")
+	// Involucro do elemento
+	@WebResult(name = "item")
+	public List<Item> getItens(@WebParam(name = "filtros") Filtros filtros) {
+		System.out.println("Buscando todos os itens com filtro");
+
+		List<Filtro> lista = filtros.getLista();
+		ArrayList<Item> itensFiltrados = this.itemDAO.todosItens(lista);
+
+		return itensFiltrados;
 	}
 }
